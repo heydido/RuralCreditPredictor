a5ecce3dc3bbe525c151d1316ee46907b4f64d39
heydido <aashish4.iitd@gmail.com>
2024-03-19 22:08:35 +0530

    fix typo

--- template.py
+++ template.py
@@ -19,9 +19,9 @@ list_of_files = [
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "dvc.yaml",
-    "notebook/.gitkeep",
+    "notebook/experiment.ipynb",
     "notebook/data/.gitkeep",
-    "params2.yaml",
+    "params.yaml",
     "requirements.txt",
     "setup.py",
     "templates/index.html"
