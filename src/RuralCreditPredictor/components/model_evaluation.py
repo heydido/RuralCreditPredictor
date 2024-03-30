7727110c6d8d90cc859a05e4c2d4a65317d71e32
heydido <aashish4.iitd@gmail.com>
2024-03-30 17:38:58 +0530

    refactor to add metrics to same run_id as params

--- src/RuralCreditPredictor/components/model_evaluation.py
+++ src/RuralCreditPredictor/components/model_evaluation.py
@@ -37,47 +37,71 @@ class ModelEvaluator:
                 logging.error(f"Error occurred while evaluating model!")
             raise CustomException(e, sys)
 
-    def get_model_metrics(self):
+    def _get_run_id_and_model(self):
         try:
-            logging.info("> Getting model metrics:")
+            logging.info("> Getting model:")
 
-            logging.info(f"Loading run_id to track model metrics:")
             with open(self.config.latest_run_id, 'r') as file:
                 run_id = file.read().strip()
                 logging.info(f"run_id: {run_id}")
 
-            with mlflow.start_run(run_id=run_id):
-                tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+            # Note: Comment below two lines to run do prediction using a local model
+            remote_server_uri = "https://dagshub.com/heydido/RuralCreditPredictor.mlflow"
+            mlflow.set_tracking_uri(remote_server_uri)
 
-                if tracking_url_type_store != "file":
-                    model = mlflow.sklearn.load_model("models:/RandomForestModel/None")  # None to pull the latest model
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
-                else:
+            if tracking_url_type_store != "file":
+                logging.info("Mode - Remote")
+
+                model_uri = f"runs:/{run_id}/model"
+                model = mlflow.sklearn.load_model(model_uri)
+
+                logging.info(f"Model loaded successfully from: {model_uri}")
+
+                return run_id, model
+
+            else:
+                logging.info("Mode - Local")
+
+                with mlflow.start_run(run_id=run_id):
                     experiment_id = mlflow.get_experiment_by_name(self.config.experiment_name).experiment_id
                     model_path = f"mlruns/{experiment_id}/{run_id}/artifacts/model"
                     model = mlflow.sklearn.load_model(model_path)
 
-                config = ConfigurationManager()
-                model_training_config = config.get_model_training_config(log=False)
-                trainer = ModelTrainer(config=model_training_config)
+                    logging.info(f"Model loaded successfully from: {model_path}")
 
-                x_train, x_test, y_train, y_test = trainer.get_data(log=False)
+                    return run_id, model
 
-                y_pred_train = model.predict(x_train)
-                y_pred_test = model.predict(x_test)
+        except Exception as e:
+            logging.error(f"Error occurred while getting model!")
+            raise CustomException(e, sys)
 
-                mae_train, mape_train, mse_train, rmse_train = self.evaluate_model(y_train, y_pred_train, log=False)
+    def get_model_metrics(self):
+        try:
+            logging.info("> Getting model metrics:")
 
+            run_id, model = self._get_run_id_and_model()
+
+            config = ConfigurationManager()
+            model_training_config = config.get_model_training_config(log=False)
+            trainer = ModelTrainer(config=model_training_config)
+
+            x_train, x_test, y_train, y_test = trainer.get_data(log=False)
+
+            y_pred_train = model.predict(x_train)
+            y_pred_test = model.predict(x_test)
+
+            mae_train, mape_train, mse_train, rmse_train = self.evaluate_model(y_train, y_pred_train, log=False)
+            mae_test, mape_test, mse_test, rmse_test = self.evaluate_model(y_test, y_pred_test, log=False)
+
+            with mlflow.start_run(run_id=run_id):
                 mlflow.log_metric("mae_train", mae_train)
                 mlflow.log_metric("mape_train", mape_train)
                 mlflow.log_metric("mse_train", mse_train)
                 mlflow.log_metric("rmse_train", rmse_train)
 
-                logging.info(
-                    f"Train metrics: MAE: {mae_train}, MAPE: {mape_train}, MSE: {mse_train}, RMSE: {rmse_train}"
-                )
-
-                mae_test, mape_test, mse_test, rmse_test = self.evaluate_model(y_test, y_pred_test, log=False)
+                logging.info(f"Train metrics: MAE: {mae_train}, MAPE: {mape_train}, MSE: {mse_train}, RMSE: {rmse_train}")
 
                 mlflow.log_metric("mae_test", mae_test)
                 mlflow.log_metric("mape_test", mape_test)
@@ -86,36 +110,41 @@ class ModelEvaluator:
 
                 logging.info(f"Test metrics: MAE: {mae_test}, MAPE: {mape_test}, MSE: {mse_test}, RMSE: {rmse_test}")
 
-                train_metrics = json.dumps(
-                    {
-                        "mae": mae_train,
-                        "mape": mape_train,
-                        "mse": mse_train,
-                        "rmse": rmse_train
-                    },
-                    indent=4
-                )
+            logging.info("Model metrics logged successfully! Ending run....")
 
-                test_metrics = json.dumps(
-                    {
-                        "mae": mae_test,
-                        "mape": mape_test,
-                        "mse": mse_test,
-                        "rmse": rmse_test
-                    },
-                    indent=4
-                )
+            # End Run
+            mlflow.end_run()
 
-                train_metrics_path = self.config.train_metrics
-                test_metrics_path = self.config.test_metrics
+            train_metrics = json.dumps(
+                {
+                    "mae": mae_train,
+                    "mape": mape_train,
+                    "mse": mse_train,
+                    "rmse": rmse_train
+                },
+                indent=4
+            )
 
-                with open(train_metrics_path, 'w') as file:
-                    file.write(train_metrics)
+            test_metrics = json.dumps(
+                {
+                    "mae": mae_test,
+                    "mape": mape_test,
+                    "mse": mse_test,
+                    "rmse": rmse_test
+                },
+                indent=4
+            )
 
-                with open(test_metrics_path, 'w') as file:
-                    file.write(test_metrics)
+            train_metrics_path = self.config.train_metrics
+            test_metrics_path = self.config.test_metrics
 
-                logging.info(f"Model metrics are ready. Saved at: {train_metrics_path}, {train_metrics_path}!")
+            with open(train_metrics_path, 'w') as file:
+                file.write(train_metrics)
+
+            with open(test_metrics_path, 'w') as file:
+                file.write(test_metrics)
+
+            logging.info(f"Model metrics are ready. Saved at: {train_metrics_path}, {train_metrics_path}!")
 
         except Exception as e:
             logging.error(f"Error occurred while getting model metrics!")
